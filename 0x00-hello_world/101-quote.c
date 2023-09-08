15300128a2351edaabb29aa52b251b44147c5ce4
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-08 16:08:39 +0000

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: this is set to 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+	fprintf(stderr, "2015-10-19\n");
+
+	return (0);
+}
