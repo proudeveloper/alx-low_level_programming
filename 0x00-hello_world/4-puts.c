5d2dc2c701e82794c0b0969dcbe48e2ee7684914
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-08 12:53:56 +0000

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function where other functions are found
+ *
+ * Return: this return is set to 0
+ */
+
+int main(void)
+{
+	puts("\ "Programming is like building a multilingual puzzle");
+	return (0);
+}
