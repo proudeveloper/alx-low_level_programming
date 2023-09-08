ee764246d884c3e953b60194835cd4d5f93240d5
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-08 13:20:59 +0000

    Printf with a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  this is the main function where all other functions are contained
+ *
+ * Return: this return is set to 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0);
+}
