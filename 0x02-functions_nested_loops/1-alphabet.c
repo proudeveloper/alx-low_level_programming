c857a8a627fe6cf7224cea2a26a988817eedeeb9
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-12 22:58:41 +0000

    alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - this function prints alphabets in lower case
+ *
+ * Return: the return is set to 0
+ */
+
+void print_alphabet(void)
+
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+
+		_putchar(i);
+	_putchar('\n');
+
+}
