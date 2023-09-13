f68c7c635a8e9185cfc5e2f792fbab2a186173d6
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-13 20:06:22 +0000

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,10 +13,10 @@ int main(void)
 	unsigned long int next;
 	int i;
 
-	printf("%d, ", parameter1);
+	printf("%lu, ", parameter1);
 	for (i = 1 ; i < 50 ; i++)
 	{
-		printf("%d, ", parameter2);
+		printf("%lu, ", parameter2);
 		next = parameter1 + parameter2;
 		parameter1 = parameter2;
 		parameter2 = next;
