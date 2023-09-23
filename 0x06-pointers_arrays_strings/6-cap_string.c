1dcce7aae0820e533d3a2fe022fd170fcdfe8909
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-23 23:20:55 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to be tranformed
+ * Return: char*
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+
+	cap = 1;
+
+	char *sep = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (cap && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] + 'A' - 'a';
+		}
+		cap = 0;
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			cap = 1;
+			break;
+		}
+	}
+	return (s);
+}
