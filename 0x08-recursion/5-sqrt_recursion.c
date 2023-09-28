ba18a3557693fb4846e0e34daab9418c5398933a
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-28 01:15:42 +0000

    5-sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - this returns the natural square root of a number
+ * @n: this is a parameter
+ * Return: void
+ */
+int _sqrt_recursion(int n)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - this returns the natural square root of a number
+ * @n: this is a parameter
+ * Return: void
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt_helper(n, 0));
+}
