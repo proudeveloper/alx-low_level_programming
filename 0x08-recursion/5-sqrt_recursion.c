f6b66c749316283c72ad366d1134eaf7ff65d05a
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-09-28 01:18:54 +0000

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,9 +3,10 @@
 /**
  * _sqrt_recursion - this returns the natural square root of a number
  * @n: this is a parameter
+ * @i: second parameter
  * Return: void
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int i)
 {
 	if (i * i == n)
 		return (i);
