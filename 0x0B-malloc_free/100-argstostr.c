63f24b83fedbf9162f4818cba465b97eb948cc41
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-10-03 13:04:17 +0000

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -29,4 +29,21 @@ char *argstostr(int ac, char **av)
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL)
 		return (NULL);
+
+	for (i = 0; j = 0; i < ac && j < len; i++)
+	{
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n',
+	}
+	str[j] = '\0';
+
+	return (str);
 }
