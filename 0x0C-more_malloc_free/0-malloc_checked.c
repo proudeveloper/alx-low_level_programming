ee26cd6e54017aa9d7234dea0b378eeb04eef25b
Dennis Boakye Yiadom <dennisboakye64@yahoo.com>
2023-10-05 11:38:07 +0000

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include "stdlib.h"
 
 /**
- * malloc_checked cause normal process termination with a status value of 98
+ * malloc_checked - cause normal process termination with a status value of 98
  *
  * @b: allocated memory
  *
