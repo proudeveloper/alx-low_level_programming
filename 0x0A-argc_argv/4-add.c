f5925e9a151af87306d7588fa379b5fad59151ca
Dennis_Boakye_Yiadom <dennisboakye64@yahoo.com>
2023-10-30 21:20:22 +0000

    4-add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, num, sum = 0;
+
+	if (argc == 1)
+	{
+		puts("0");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[j][i] != '\0'; j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					puts("Error");
+					return (1);
+				}
+			}
+			num = atoi(argv[i]);
+			sum += num;
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
