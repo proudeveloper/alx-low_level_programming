b3dbe402c5478e48d4c9effa9a65f4623cf0b06a
Dennis_Boakye_Yiadom <dennisboakye64@yahoo.com>
2023-10-30 16:52:00 +0000

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * followed by a new line
+ * @argc: argument count
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	
+	printf("%d\n", argc);
+
+	return (0);}
