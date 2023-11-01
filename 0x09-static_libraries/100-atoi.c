c4923f3e167a9f77b4f042c9973025bf1e2a51e4
Dennis_Boakye_Yiadom <dennisboakye64@yahoo.com>
2023-11-01 16:12:23 +0000

    static_libraries updated

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,29 +1,14 @@
 #include "main.h"
 
 /**
- * _antoi - Changes a string to an integer
- * @s: The string to be changed
+ * _atoi - atoi
+ * @s: char*
  *
- * Return: The integer value of the converted  string.
+ * Return: 1
  */
 
-int _antoi(char *s)
+int _atoi(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
-
-	do {
-		if (*s == '-')
-			sign *= -1;
-
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
-	return (num * sign);
+	return (1);
 
 }
