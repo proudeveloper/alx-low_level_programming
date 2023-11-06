f3dac701bb308c4f638b972b3377505268331a73
Dennis_Boakye_Yiadom <dennisboakye64@yahoo.com>
2023-11-06 23:52:54 +0000

    3-main.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,5 +13,5 @@ void print_dog(struct dog *d)
 
 	printf("Name: %s\n", (d->name == NULL ? "(nil)" : d->name));
 	printf("Age: %f\n", d->age);
-	printf("Owner:%s\n", (d->owner == NULL ? "(nil)" : d->owner));
+	printf("Owner: %s\n", (d->owner == NULL ? "(nil)" : d->owner));
 }
